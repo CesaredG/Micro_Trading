86f6199653d38a84893de1e73752fe13ac2e1491
Ricardo <if722544@iteso.mx>
2024-02-18 19:10:34 -0600

    Se agrega la clase Operations en el archivo utils

---
+++ Proyecto 2/utils/utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Operation:
+    def __init__(self, operation_type, bought_at, timestamp, n_shares,
+                 stop_loss,take_profit):
+        self.operation_type = operation_type
+        self.bought_at = bought_at
+        self.timestamp = timestamp
+        self.n_shares = n_shares
+        self.sold_at =  None
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
+
